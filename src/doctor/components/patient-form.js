b8b759ddc39658a212db1ffa6f69de3de789e39e
Pop Claudia Dorina <pop_claudia_dorina@yahoo.com>
2020-11-11 09:18:43 +0000

    Update patient-form.js

--- src/doctor/components/patient-form.js
+++ src/doctor/components/patient-form.js
@@ -23,7 +23,7 @@ class PatientForm extends React.Component {
             tableData: null,
             options: [{value: null, label: "Select caregiver..."}],
             formIsValid: this.props.action === 'update',
-            caregiv: 0,
+            caregiv: null,
             formControls: {
                 name: {
                     value: this.props.action==='update'? this.props.name : null,
