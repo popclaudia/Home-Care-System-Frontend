e7ec7d12179feb3e97fe00eec53ea687876c83a0
Pop Claudia Dorina <pop_claudia_dorina@yahoo.com>
2020-11-18 21:52:42 +0000

    Update hosts.js

--- src/commons/hosts.js
+++ src/commons/hosts.js
@@ -1,4 +1,4 @@
 export const HOST = {
-    backend_api: 'http://localhost:8080',
-    //backend_api: 'https://spring-assignment-1.herokuapp.com',
+    //backend_api: 'http://localhost:8080',
+    backend_api: 'https://spring-assignment-1.herokuapp.com',
 };
